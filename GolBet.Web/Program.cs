0adc99fd05b0d5c1c3850784b25753c1cf27c89f
Santiago Quintero López <santiagoquintero.developer@gmail.com>
2026-09-10 17:58:39 -0500

    Add generic repository, match repository and database seeder (Module 3)

--- GolBet.Web/Program.cs
+++ GolBet.Web/Program.cs
@@ -1,5 +1,8 @@
 using GolBet.Repositories.Data;
 using Microsoft.EntityFrameworkCore;
+using GolBet.Repositories.Implementations;
+using GolBet.Repositories.Interfaces;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +17,28 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
+// Specific repositories 
+
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
+
+// Open generic registration: one line, a repository for every entity 
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
 var app = builder.Build();
+
+// Seed the database on startup 
+
+using (var scope = app.Services.CreateScope())
+
+{
+
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    await DbSeeder.SeedAsync(context);
+
+}
+
 
 
 
